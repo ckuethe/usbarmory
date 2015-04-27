8d958761461bdd755a5917e246b47003fe90d168
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-04-26 17:49:38 -0700

    Tools for converting a boot rom dump to ELF
    Allows rom extracts to be easily loaded with Hopper (or IDA) for easy
    analysis

---
+++ misc/bootromlink.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+X="arm-linux-gnueabihf-"
+E="mx53boot.elf"
+
+rm -f empty.o bootrom-*.o $E
+
+echo | ${X}as -o empty.o
+cp empty.o bootrom-0.o
+cp empty.o bootrom-1.o
+
+F="--set-section-flags .blob=alloc,contents,load,code"
+${X}objcopy --add-section .blob=bootrom-0-16k.bin $F bootrom-0.o 
+${X}objcopy --add-section .blob=bootrom-1-48k.bin $F bootrom-1.o 
+
+${X}ld -e 0xc0 -T simplescript.ld -o $E
