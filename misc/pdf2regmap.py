647e24e33adbdb632bed985af399919761ab1905
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-05-12 23:56:43 -0700

    extract register descriptions from reference pdf

---
+++ misc/pdf2regmap.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# vim: tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab:
+
+# Copyright: (c)2015 Chris Kuethe <chris.kuethe+github@gmail.com>
+# License: Perl Artistic <http://dev.perl.org/licenses/artistic.html>
+# Description: generate a register map from the reference manual
+
+import re
+import PyPDF2
+import sys
+
+if len(sys.argv) != 3:
+	print "usage: %s <pdfsrc> <outfile>" % sys.argv[0]
+	sys.exit(1)
+
+infp = open(sys.argv[1], 'rb')
+outfp = open(sys.argv[2], 'w')
+pdf = PyPDF2.PdfFileReader(infp)
+rgx = re.compile('Address: ([A-Z0-9_-]+) is [0-9A-F]{4}_[0-9A-F]{4}h.*? = ([0-9A-F]{4})_([0-9A-F]{4})h')
+
+for i in range( pdf.getNumPages() ):
+	sys.stderr.write("%d " % i)
+	pg = pdf.getPage(i)
+	txt = pg.extractText()
+
+	for match in rgx.findall(txt):
+		addr = int("%s%s" % (match[1], match[2]), 16)
+		reg = "%d %s\n" % (addr, match[0])
+		#print reg
+		outfp.write(reg)
+
+outfp.close()
