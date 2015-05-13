11fb5f038d3ef7e38574feec114334cae4ef472a
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-05-12 22:23:05 -0700

    add debug and platform registers

--- misc/bootromlink.sh
+++ misc/bootromlink.sh
@@ -13,6 +13,8 @@ cat <<EOF | ${X}gcc -x c -c - -o vars.o
 #include <stdint.h>
 
 uint8_t __attribute__((section(".scram"))) _scram[32*1024] ; // includes alias
+uint8_t __attribute__((section(".debug"))) _debug[36*1024] ;
+uint8_t __attribute__((section(".sbpa"))) _sbpa[256*1024] ;
 uint8_t __attribute__((section(".tzram"))) _tzram[16*1024] ;
 uint8_t __attribute__((section(".regs0"))) _regs0[0x80000] ;
 uint8_t __attribute__((section(".regs1"))) _regs1[0x80000] ;
